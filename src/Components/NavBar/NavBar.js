4eb887bf0cd5738b2544fe10dc2ba92ec63695ad
emilysarina <emy.sarina@gmail.com>
2022-02-10 22:24:24 +0000

    added nav bar css

--- src/Components/NavBar/NavBar.js
+++ src/Components/NavBar/NavBar.js
@@ -4,6 +4,10 @@ import { Link } from "react-router-dom";
 
 export default function NavBar() {
   return <div className={css.NavBar}>
+      <Link to="/">
+      <img src='https://i.ibb.co/7NTChCw/E-9.png' alt='logo' className={css.logo}/>
+      </Link> 
+      <div className={css.pageButtons}>
       <Link to="about">
       <p className={css.NavButton}>
       About.</p></Link>
@@ -15,12 +19,13 @@ export default function NavBar() {
       <p className={css.NavButton}>
       Contact.</p>
       </Link>
+      </div>
   </div>;
 }
 
 
 /*
-     <img src='https://i.ibb.co/R6JZnjM/E.png' alt='logo' className={css.logo}/>
+     <img src='https://i.ibb.co/Hp9wdZM/E-6.png' alt='logo' className={css.logo}/>
 .logo {
     display: flex;
     justify-content: left;
