8efa3de6fabc7f8ecb93ac05c8bea16c1d9b6a8b
emilysarina <emy.sarina@gmail.com>
2022-02-23 23:03:45 +0000

    changed images to local

--- src/Pages/HomePage/Home.js
+++ src/Pages/HomePage/Home.js
@@ -15,10 +15,10 @@ export default function Home() {
       </p>
       
       <button className={css.cvButton}>
-      <img src='/cvButton.jpeg' alt='logo' className={css.cvIcon}/>
+      <img src='/cvlogo.png' alt='logo' className={css.cvIcon}/>
       </button>
       <button className={css.socialButton}>
-      <img src='https://i.ibb.co/mJNF2nF/pink-linkedin.png' alt='logo' className={css.logo}/>
+      <img src='/linkedin.png' alt='logo' className={css.logo}/>
       </button>
       </div>
 
