905ce1012616db54621580621430cb17ccf806e5
emilysarina <emy.sarina@gmail.com>
2022-02-26 22:55:36 +0000

    added first two project cards

---
+++ src/Components/ProjectCard/ProjectCard.js
@@ -0,0 +1,50 @@
+import React from 'react'
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Typography from '@mui/material/Typography';
+import { CardActionArea, CardActions } from '@mui/material';
+
+
+
+export default function ProjectCard({projectImg, altText, title, body, projectWebsite}) {
+  return (
+    <Card sx={{ maxWidth: 345, bgcolor:'#495867'}}>
+    <CardActionArea>
+      <CardMedia sx={{ bgcolor:'#495867', display: 'inline', border:'2px'}}
+        component="img"
+        height="140"
+        image={projectImg}
+        alt={altText}
+      />
+      <CardContent height='10em'>
+        <Typography 
+         gutterBottom variant="h5"
+         component="div" 
+         lineHeight='1em'
+         sx={{color:'#ecc8af', fontFamily:'Quicksand', textAlign:'left'}}>
+          {title}
+        </Typography>
+        <Typography variant="body2" sx={{ color:'#ecc8af', fontFamily:'Quicksand', textAlign:'left'}}>
+          {body}
+        </Typography>
+        <Typography variant="body2" sx={{ color:'#ecc8af', fontFamily:'Quicksand', textAlign:'left'}}>
+        </Typography>
+      </CardContent>
+    </CardActionArea>
+    <CardActions>
+    <Typography variant="body2" sx={{ color:'#ecc8af', fontFamily:'Quicksand', paddingLeft:'0.5em', fontWeight:'bold'}}>
+    <a href={projectWebsite} 
+       rel="noreferrer" 
+       target="_blank">Visit project here</a>
+    </Typography>
+    </CardActions>
+  </Card>
+  )
+}
+
+
+
+
+
+ 
--- src/Pages/ProjectsPage/Projects.js
+++ src/Pages/ProjectsPage/Projects.js
@@ -1,8 +1,10 @@
 import React from 'react'
 import css from './projects.module.css';
 import Title from '../../Components/Title/Title';
+import ProjectCard from '../../Components/ProjectCard/ProjectCard';
 
 
+//this is the projects page 
 
 export default function Projects() {
   return (
@@ -10,6 +12,20 @@ export default function Projects() {
     <div className={css.projectTitle}>
     <Title text={"Projects"}  />
     </div>
+    <ProjectCard 
+    projectImg='/AudreySite2.png' 
+    altText={'Audrey Hepburn Site'} 
+    title={"Precourse Project"} 
+    body={"Before I started The School of Code Bootcamp all bootcamper had to make a site that passed the 'freecodecamps' tribute page test's. I did mine on Audery Hepburn. This was the first coding project I ever made."}
+    projectWebsite={'https://pre-course-project.netlify.app'}  
+    />
+    <ProjectCard 
+    projectImg='/FeedbackSite.png' 
+    altText={'Feedback Site'} 
+    title={"One Week Project"} 
+    body={"On week nine of the bootcamp we worked in a team to solve a problem for the bootcamp. Our team decided to make a bootcamper portal with a simplified feedback form and a help section."}
+    projectWebsite={'https://heuristic-johnson-b3e4f3.netlify.app'}  
+    />
     </div>
   )
 }
